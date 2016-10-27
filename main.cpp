c74739815f099a347af383c07116f3f5e5fcd0d5
Matt Neal <mattneal@umail.iu.edu>
2016-10-27 00:34:54 -0400

    Consistency and readability spacing changes

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@
 //struct to handle all user-based information
 //handles a users id and coins so that they are not global
 struct userID {
-    void setCoins(int coin) {
+    void setCoins (int coin) {
         coins = coin;
     }
 
-    int getCoins(){
+    int getCoins () {
         return coins;
     }
 
-    void setUserId(std::string user) {
+    void setUserId (std::string user) {
         userId = user;
     }
 
@@ -23,7 +23,7 @@ public:
 };
 
 //number generator with a passed in user-defined range
-int NewNumber(int range) {
+int NewNumber (int range) {
     int random = rand() %range;
     return random;
 }
@@ -34,7 +34,7 @@ int NewNumber(int range) {
 //Randomly generates 2 numbers, one to find, one as a guess
 //Regenerates guesses until it matches the find.
 //Increments coin count when match is found.
-bool HandleGuess(struct userID * newUser, int range) {
+bool HandleGuess (struct userID * newUser, int range) {
     bool keepGoing = true;
     int guesses = 1;
     int numberToFind = NewNumber(range);
@@ -56,14 +56,14 @@ bool HandleGuess(struct userID * newUser, int range) {
 }
 
 //returns the user structs current amount of coins
-int GetCoins(struct userID * newUser){
+int GetCoins (struct userID * newUser) {
     int coinInfo = newUser->getCoins();
     std::cout << coinInfo << std::endl;
     return coinInfo;
 }
 
 //begins the guessing program
-bool StartGuessing(int range, struct userID * newUser) {
+bool StartGuessing (int range, struct userID * newUser) {
     HandleGuess(newUser, range);
     return true;
 }
@@ -75,7 +75,7 @@ bool StartGuessing(int range, struct userID * newUser) {
 //Includes minor error catching for mismatched input values to expected type or range.
 //Returns current number of coins a user has, which resets upon struct delete.
 int main(){
-    srand(time(NULL));
+    srand (time(NULL));
     bool keepgoing = true;
     bool keepGOING = true;
     int choose = 0;
@@ -84,22 +84,26 @@ int main(){
     while (keepgoing) {
         std::cout << "Welcome to XternCoin!" << std::endl;
         std::cout << "Please enter a user id" << std::endl;
-        keepGOING = true;
-        std::cin >> userId;
 
+        keepGOING = true;
+
+        std::cin >> userId;
         userID *newUser = new userID;
         newUser->setUserId(userId);
+
         while (keepGOING) {
             std::cout << "Please select from the following options:" << std::endl;
             std::cout << "1 to start a new random guess." << std::endl;
             std::cout << "2 to clear your user information." << std::endl;
             std::cout << "3 to end your session. This will terminate the program." << std::endl;
             std::cin >> choose;
+
             if (choose == 1) {
                     std::cout << "Please select a range for the random number and search." << std::endl;
                     std::cout << "(I advise less than 100000.)" << std::endl;
                     std::cin >> range;
-                    if(std::cin.fail() && range > 9999999){
+
+                    if (std::cin.fail() or range > 9999999) {
                         std::cout << "I'm sorry, that is an invalid selection." << std::endl;
                         std::cin.clear();
                     }
@@ -107,14 +111,17 @@ int main(){
                         StartGuessing(range, newUser);
                         GetCoins(newUser);
                     }
+
                 }
             else if (choose == 2) {
                 delete newUser;
                 keepGOING = false;
-            } else if (choose == 3) {
+            }
+            else if (choose == 3) {
                 keepgoing = false;
                 keepGOING = false;
-            } else {
+            }
+            else {
                 std::cout << "I'm sorry, that is an invalid selection." << std::endl;
             }
         }
